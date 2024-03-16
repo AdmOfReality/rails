b1186db97d2152ad160291cb0d473525a2224bce
AdmOfReality <angrysgr@yandex.kz>
2024-03-16 18:00:40 +0500

    Добавил ассоциации по ER диграмме

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,11 +1,10 @@
 class Test < ApplicationRecord
-  # belongs_to :user
+  belongs_to :category
   has_many :questions
-  # has_and_belongs_to_many :users
   has_many :tests_users
   has_many :users, through: :tests_users
 
-  # def self.sort_desc_test_name_by_category(title)
-  #   Test.joins("INNER JOIN categories ON tests.category_id = categories.id").where(categories: {title: title}).order(title: :desc).pluck(:title)
-  # end
+  def self.sort_desc_test_name_by_category(title)
+    Test.joins("INNER JOIN categories ON tests.category_id = categories.id").where(categories: {title: title}).order(title: :desc).pluck(:title)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,9 @@
 class User < ApplicationRecord
-  # has_many :tests
   has_one :payment_account
-  # has_and_belongs_to_many :tests
   has_many :tests_users
   has_many :tests, through: :tests_users
 
-  # def show_list_tests_by_level(level)
-  #   Test.joins("INNER JOIN users on (tests.user_id = users.id)").where(tests: {level: level})
-  # end
+  def show_list_tests_by_level(level)
+    Test.joins("INNER JOIN users on (tests.user_id = users.id)").where(tests: {level: level})
+  end
 end
