acf5230369333ca6ee0225f9d36090be200bb3e8
AdmOfReality <angrysgr@yandex.kz>
2024-03-16 20:08:10 +0500

    fix v5

--- app/models/test.rb
+++ app/models/test.rb
@@ -9,3 +9,4 @@ class Test < ApplicationRecord
     Test.joins("INNER JOIN categories ON tests.category_id = categories.id").where(categories: {title: title}).order(title: :desc).pluck(:title)
   end
 end
+
