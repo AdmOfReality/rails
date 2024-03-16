10098d9863b6c1c7117b58c59be7c06cfcadf3bb
AdmOfReality <angrysgr@yandex.kz>
2024-03-16 19:26:46 +0500

    Fix methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   has_many :tests, through: :tests_users
 
   def show_list_tests_by_level(level)
-    Test.joins("INNER JOIN users on (tests.user_id = users.id)").where(tests: {level: level})
+    Test.joins("INNER JOIN users on (tests.owner = users.id)").where(tests: {level: level})
   end
 end
