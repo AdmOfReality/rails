786e627f4c42c9aff8b30ad4b885c5a41f1d34fb
AdmOfReality <111571212+AdmOfReality@users.noreply.github.com>
2024-03-25 20:04:54 +0500

    Fix comments

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -4,4 +4,10 @@ class Answer < ApplicationRecord
   scope :correct, -> { where(correct: true)}
 
   validates :body, presence: true
+
+  validate :validate_count_of_answers, on: :create
+
+  def validate_count_of_answers
+    errors.add(:base, "max answers is 4") if question.answers.count >= 4
+  end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -3,10 +3,4 @@ class Question < ApplicationRecord
   has_many :answers
 
   validates :body, presence: true
-
-  validate :validate_count_of_answers, on: :create
-
-  def validate_count_of_answers
-    errors.add(:base, :answers, "max answers is 4") if answers.count >= 4
-  end
 end
