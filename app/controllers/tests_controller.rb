9a63c103aa780849f73d7771967d6a25be3c508a
AdmOfReality <111571212+AdmOfReality@users.noreply.github.com>
2024-04-11 23:05:02 +0500

    Finish develop by videos

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,19 +1,51 @@
 class TestsController < ApplicationController
 
-  before_action :find_test, only: :show
+  before_action :find_test, only: [:show, :destroy, :update, :edit]
 
   def index
-    render plain: 'All tests'
+    @tests = Test.all
+  end
+
+  def new
+    @test = Test.new
+  end
+
+  def edit
+  end
+
+  def create
+    @test = Test.new(test_params)
+    if @test.save
+      redirect_to @test
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @test.update(test_params)
+      redirect_to @test
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @test.destroy
+    redirect_to tests_path
   end
 
   def show
-    render inline: '<%= @test.title %>'
   end
 
   private
 
   def find_test
     @test = Test.find(params[:id])
+  end
+
+  def test_params
+    params.require(:test).permit(:title, :level, :category_id, :owner_id)
   end
 
 end
