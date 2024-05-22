de871fe16f5a3ec34554c7187b7d771881d5bc39
AdmOfReality <111571212+AdmOfReality@users.noreply.github.com>
2024-05-22 14:51:01 +0500

    Add before_validation to next_question

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -5,6 +5,7 @@ class TestPassage < ApplicationRecord
   belongs_to :current_question, class_name: 'Question', optional: true
 
   before_validation :before_validation_set_first_question, on: :create
+  before_validation :before_validation_set_next_question, on: :update
 
   def completed?
     current_question.nil?
@@ -15,7 +16,7 @@ class TestPassage < ApplicationRecord
       self.correct_questions += 1
     end
 
-    self.current_question = next_question
+
     save!
   end
 
@@ -23,6 +24,10 @@ class TestPassage < ApplicationRecord
 
   def before_validation_set_first_question
     self.current_question = test.questions.first if test.present?
+  end
+
+  def before_validation_set_next_question
+    self.current_question = next_question if new_record? == false
   end
 
   def correct_answer?(answer_ids)
