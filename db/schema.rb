1b5ad02536a72c33f94eac1eeb9fe10a17cb2db7
AdmOfReality <111571212+AdmOfReality@users.noreply.github.com>
2024-05-22 13:34:27 +0500

    End develop

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_22_110529) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_21_115752) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,7 +40,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_22_110529) do
   create_table "test_passages", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "test_id", null: false
-    t.bigint "current_question_id", null: false
+    t.bigint "current_question_id"
     t.integer "correct_questions", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
