3771a144c4c4d68d5323bb93bdf7c442ef924e2f
AdmOfReality <111571212+AdmOfReality@users.noreply.github.com>
2024-08-20 11:34:51 +0500

    heroku-mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,6 +74,18 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "rails_exercise_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'rails-app-aor-6fd107a8f507.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    user_name:            ENV['SMTP_USERNAME'],
+    password:             ENV['SMTP_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
+
+  config.action_mailer.raise_delivery_errors = true
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
